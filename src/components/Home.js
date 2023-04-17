a91e9457ec0c0060584fd008276ee16868f3fd1e
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2023-04-16 23:49:48 -0700

    progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import prompPic from "../assets/croppedprompic.jpg";
 
 const variants = {
   show: {
@@ -31,37 +32,53 @@ function Home() {
 
   return (
     <div name="home" className="w-full h-screen bg-[#3b3a37]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-white font-bold text-3xl">Yo! I'm... </p>
-        <div className="flex flex-row mt-5 ">
-          {"Siddhanth".split("").map((letter, index) => {
-            return (
-              <span
-                key={index}
-                className=" hover:-mt-2 transition-all duration-500 hover:duration-100 text-green-500 text-3xl sm:text-7xl font-bold hover:text-white"
-              >
-                {letter}
-              </span>
-            );
-          })}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+        <div className="flex flex-row">
+          <div>
+            <p className="text-white font-bold text-3xl">Yo! I'm... </p>
+            <div className="flex flex-row mt-5 ">
+              {"Siddhanth".split("").map((letter, index) => {
+                return (
+                  <span
+                    key={index}
+                    className=" hover:-mt-2 transition-all duration-500 hover:duration-100 text-yellow-500 text-3xl sm:text-7xl font-bold hover:text-white"
+                  >
+                    {letter}
+                  </span>
+                );
+              })}
 
-          <p className="text-3xl sm:text-7xl font-bold text-green-500 animate-bounce">
-            🍏
-          </p>
+              <p className="text-3xl sm:text-7xl font-bold text-yellow-500 animate-bounce">
+                🙂
+              </p>
+            </div>
+            <p className="text-3xl font-bold text-white mt-5">
+              Incoming CS @ <span className="text-yellow-500">Purdue</span>
+            </p>
+            <p className="text-3xl font-bold text-white mt-5">
+              I am a{" "}
+              <motion.span
+                className="text-yellow-500"
+                key={adjective}
+                variants={variants}
+                animate={"show"}
+                initial="hide"
+              >
+                {adjective}
+              </motion.span>{" "}
+            </p>
+            <p className="text-3xl font-bold text-white mt-1">
+              -CS enthusiast {<br />} -high schooler {<br />} -aspiring SWE
+            </p>
+          </div>
+          <div>
+            <img
+              src={prompPic}
+              alt="picture of siddhanth kumar"
+              className="rounded-full object-cover"
+            />
+          </div>
         </div>
-        <p className="text-3xl font-bold text-white mt-5">
-          I am a{" "}
-          <motion.span
-            className="text-green-500"
-            key={adjective}
-            variants={variants}
-            animate={"show"}
-            initial="hide"
-          >
-            {adjective}
-          </motion.span>{" "}
-          CS enthusiast, high schooler, and aspiring SWE.
-        </p>
       </div>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,12 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="fixed w-full h-20 flex justify-between items-center px-4 text-lg bg-[#3b3a37]">
-      <div className="flex text-green-500 font-bold text-2xl">SKUMAR.</div>
+      <div className="flex text-yellow-500 font-bold text-2xl">SKUMAR.</div>
       <div>
         <ul className="flex text-white">
-          <li className="transition hover:text-green-500">Contact</li>
-          <li className="transition hover:text-green-500">Work</li>
-          <li className="transition hover:text-green-500">About Me</li>
+          <li className="transition hover:text-yellow-500">Contact</li>
+          <li className="transition hover:text-yellow-500">Work</li>
+          <li className="transition hover:text-yellow-500">About Me</li>
         </ul>
       </div>
     </div>
