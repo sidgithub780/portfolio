a8c6414c4fead59aa55286100c87a42f31694756
skumar <53668449+sidgithub780@users.noreply.github.com>
2023-05-23 22:16:44 -0700

    FULLY MOBILE FRIENDLY

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import Intro from "./Intro";
 import AboutMe from "./AboutMe";
 import Work from "./Work";
@@ -6,6 +6,18 @@ import Skills from "./Skills";
 import Education from "./Education";
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+    toggleMenu();
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const scrollToSkills = useRef();
   const scrollToWork = useRef();
   const scrollToAbout = useRef();
@@ -13,51 +25,90 @@ function Home() {
 
   return (
     <>
-      <div className="fixed w-full h-20 flex justify-between items-center px-4 text-lg bg-[#3b3a37]">
-        <div className="flex text-yellow-500 font-bold text-2xl">SKUMAR.</div>
-        <div>
-          <ul className="flex text-white">
-            <button
-              onClick={() =>
-                scrollToAbout.current.scrollIntoView({
-                  behavior: "smooth",
-                })
-              }
+      <div className="fixed top-0 right-0 w-full h-20 px-4 text-lg bg-[#3b3a37]">
+        <div className="flex justify-between items-center h-full">
+          <div className="flex items-center text-yellow-500 font-bold text-2xl">
+            SKUMAR.
+          </div>
+          <div className="hidden md:flex">
+            <ul className="flex text-white">
+              <button onClick={() => scrollToSection(scrollToAbout)}>
+                <li className="transition hover:text-yellow-500">About Me</li>
+              </button>
+
+              <button onClick={() => scrollToSection(scrollToWork)}>
+                <li className="transition hover:text-yellow-500">Work</li>
+              </button>
+              <button onClick={() => scrollToSection(scrollToEducation)}>
+                <li className="transition hover:text-yellow-500">Education</li>
+              </button>
+              <button onClick={() => scrollToSection(scrollToSkills)}>
+                <li className="transition hover:text-yellow-500">Skills</li>
+              </button>
+            </ul>
+          </div>
+          <button
+            className={`md:hidden text-white focus:outline-none ${
+              isMenuOpen ? "bg-yellow-500" : ""
+            }`}
+            onClick={toggleMenu}
+          >
+            <svg
+              className={`w-6 h-6 fill-current ${
+                isMenuOpen ? "text-white" : "text-yellow-500"
+              }`}
+              viewBox="0 0 24 24"
             >
-              <li className="transition hover:text-yellow-500">About Me</li>
-            </button>
-            <button
-              onClick={() =>
-                scrollToWork.current.scrollIntoView({
-                  behavior: "smooth",
-                })
-              }
-            >
-              <li className="transition hover:text-yellow-500">Work</li>
-            </button>
-            <button
-              onClick={() =>
-                scrollToEducation.current.scrollIntoView({
-                  behavior: "smooth",
-                })
-              }
-            >
-              <li className="transition hover:text-yellow-500">Education</li>
-            </button>
-            <button
-              onClick={() =>
-                scrollToSkills.current.scrollIntoView({
-                  behavior: "smooth",
-                })
-              }
-            >
-              <li className="transition hover:text-yellow-500">Skills</li>
-            </button>
-          </ul>
+              {isMenuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19 7H5V5H19V7ZM19 12H5V10H19V12ZM19 17H5V15H19V17Z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 7H20V9H4V7ZM4 12H20V14H4V12ZM4 17H20V19H4V17Z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${
+            isMenuOpen ? "flex" : "hidden"
+          } md:hidden flex-col bg-[#3b3a37] rounded-lg top-20`}
+          ref={menuRef}
+        >
+          <button
+            className="block py-2 px-4 text-white transition hover:text-yellow-500"
+            onClick={() => scrollToSection(scrollToAbout)}
+          >
+            About Me
+          </button>
+          <button
+            className="block py-2 px-4 text-white transition hover:text-yellow-500"
+            onClick={() => scrollToSection(scrollToWork)}
+          >
+            Work
+          </button>
+          <button
+            className="block py-2 px-4 text-white transition hover:text-yellow-500"
+            onClick={() => scrollToSection(scrollToEducation)}
+          >
+            Education
+          </button>
+          <button
+            className="block py-2 px-4 text-white transition hover:text-yellow-500"
+            onClick={() => scrollToSection(scrollToSkills)}
+          >
+            Skills
+          </button>
         </div>
       </div>
-      <div name="home" className="w-full h-full bg-[#3b3a37]">
-        <div className="max-w-[1000px]  mx-auto px-8 flex flex-col justify-center h-full ">
+      <div className="w-full h-full bg-[#3b3a37]">
+        <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
           <Intro />
           <div ref={scrollToAbout}>
             <AboutMe />
